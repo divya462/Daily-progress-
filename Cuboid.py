5206fa6c2348002104e42b0073d75ebfbf33336b
Divya kotte <153913545+divya462@users.noreply.github.com>
2024-05-15 20:20:17 +0530

    Create Cuboid.py
    Write a python program to print the volume of cuboid

---
+++ Cuboid.py
@@ -0,0 +1,5 @@
+l=int(input("enter the length of cuboid :"))
+w=int(input("enter the width of cuboid :"))
+h=int(input("enter the height of the cuboid :"))
+volume=l*w*h
+print("volume of cuboid is :",volume)
